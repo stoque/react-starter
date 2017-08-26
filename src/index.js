594801458eb81112f825cb9c10a6323b47661ad4
Lucas Stoque <stoque@live.com>
2017-08-25 23:34:28 -0300

    Changed webpack version from 3 to 2 to run react hot loader

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './app'
 const renderApp = (NextApp) => {
   render(
     <AppContainer>
-      <App />
+      <NextApp />
     </AppContainer>,
     document.querySelector('[data-js="app"]')
   )
